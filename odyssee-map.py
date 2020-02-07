95558d41d9bcbd52064bc1ee924cab232731e190
kidonsky <bryan.pereira0@gmail.com>
2020-02-07 20:54:18 +0100

    [code struct] Split code in multiple functions.

--- odyssee-map.py
+++ odyssee-map.py
@@ -11,9 +11,9 @@ def personnalized_color(value, colorscheme):
 
     return value
 
-def main():
+def parse_parameters():
     parser = argparse.ArgumentParser()
-    
+
     parser.add_argument('-i', '--inputfile', required=True)
     parser.add_argument("-o",'--outputfile')
     parser.add_argument('-f', '--format')
@@ -21,7 +21,7 @@ def main():
 
     args = parser.parse_args()
     inputfile = args.inputfile
-    
+
     # Check format given and store it
     if args.format not in ("pdf","png","svg","jpg", "dot"):
         if args.format is not None :
@@ -40,6 +40,94 @@ def main():
     else:
         toml_file = args.configurationfile
 
+    return (inputfile, outputformat, outputfile, toml_file)    
+
+# Graph is not pretty with durations < duration_offset
+duration_offset = 9
+
+def extract_additional_infos(infos):
+    # A non-duration text was inserted between places in input file
+    if "PA" not in infos:
+        # A note about path between two places in place of duration
+        if '{' in infos :
+            notes = infos.replace('{', ' {').replace('}', '} ')
+        else :
+            notes = ' {' + infos + '} '
+        # true_duration will serve for the edge label
+        true_duration = "0PA"
+        # duration default
+        duration = duration_offset
+    else:
+        notes = ''
+        true_duration = infos
+        # To keep a visible differences between durations, 
+        # we add here the offset and then we will make a division
+        duration = int(infos.replace("PA",''))+duration_offset
+
+    return (notes, duration, true_duration)
+
+def extract_info_fromline(line):
+    line_splited = line.replace(' ', '').replace('\n', '').split("->")
+    # Check if there is a duration given on the line ?
+    if len(line_splited) == 2:
+        place_from, place_to = line_splited
+        duration = duration_offset
+        notes = ''
+        true_duration = "0PA"
+    else:
+        place_from, infos, place_to = line_splited
+        (notes, duration, true_duration) = extract_additional_infos(infos)
+
+    return (place_from, place_to, duration, true_duration, notes)
+
+def set_edge_color(duration):
+    # Set different colors in function of duration
+    if duration < 10 :
+        difficulty = "black"
+    elif duration < 15 :
+        difficulty = "grey"
+    elif duration < 20 :
+        difficulty = "orange"
+    elif duration < 30 :
+        difficulty = "red"
+    else :
+        duration = 30
+        difficulty = "brown"
+    
+    return (duration, difficulty)
+
+def create_graph_fromfile(file, graph, colors_place, colorsheme):
+    placesfrom_list = ()
+
+    for line in file:
+        # Escape comment lines from input file
+        if line[0] == '#' or line[0] == ';' or len(line) < 2 :
+            continue
+
+        (place_from, place_to, duration, true_duration, notes) = extract_info_fromline(line)
+
+        (duration, difficulty) = set_edge_color(duration)
+
+        if place_from not in placesfrom_list:
+            big_place = place_from.split(".")[0].split("_")[0] 
+            if big_place in colors_place.keys():
+                color = personnalized_color(colors_place[big_place], colorsheme)
+            else:
+                color ='white'
+            graph.node(place_from, shape="box", fillcolor=color, style='filled', color=color)
+        
+        graph.edge(place_from, place_to, 
+                label = (true_duration + notes), 
+                len = str(duration/3), 
+                weigth = str(duration), 
+                color = difficulty
+                )
+    return graph
+
+def main():
+   
+    (inputfile, outputformat, outputfile, toml_file) = parse_parameters() 
+    
     print('Input file is :', inputfile)
     print('Output file is :', outputfile)
 
@@ -48,77 +136,14 @@ def main():
     colorsheme = toml_loaded["color_sheme"]
 
     file = open(inputfile,"r")
-    placesfrom_list = ()
     places_graph = Digraph(
             comment='My Odysse Map', 
             engine='neato', 
             format=outputformat
             )
     
-    for line in file:
-        # Escape comment lines from input file
-        if line[0] == '#' or line[0] == ';' or len(line) < 2 :
-            continue
-
-        line_splited = line.replace(' ', '').replace('\n', '').split("->")
-        
-        # Check if ther is a duration given on the line ?
-        if len(line_splited) == 2:
-            place_from, place_to = line_splited
-            duration = '0PA'
-        else:
-            place_from, duration, place_to = line_splited
-
-        # Graph is not pretty with durations < duration_offset
-        duration_offset = 9
-
-        # A non-duration text was inserted between places in input file
-        if "PA" not in duration:
-            # A note about path between two places in place of duration
-            if '{' in duration :
-                notes = duration.replace('{', ' {').replace('}', '} ')
-            else :
-                notes = ' {' + duration + '} '
-            # true_duration will serve for the edge label
-            true_duration = "0PA"
-            # duration default
-            duration = str(duration_offset)
-        else:
-            notes = ''
-            true_duration = duration
-            # To keep a visible differences between durations, 
-            # we add here the offset and then we will make a division
-            duration = str(int(duration.replace("PA",''))+duration_offset)
-
-        # Set different colors in function of duration
-        if int(duration) < 10 :
-            difficulty = "black"
-        elif int(duration) < 15 :
-            difficulty = "grey"
-        elif int(duration) < 20 :
-            difficulty = "orange"
-        elif int(duration) < 30 :
-            difficulty = "red"
-        else :
-            duration = "30"
-            difficulty = "brown"
-
-
-        if place_from not in placesfrom_list:
-            big_place = place_from.split(".")[0].split("_")[0] 
-            if big_place in colors_place.keys():
-                color = personnalized_color(colors_place[big_place], colorsheme)
-            else:
-                color ='white'
-            places_graph.node(place_from, shape="box", fillcolor=color, style='filled', color=color)
-        
-        places_graph.edge(place_from, place_to, 
-                label = (true_duration + notes), 
-                len = str(int(duration)/3), 
-                weigth = duration, 
-                color = difficulty
-                )
-
+    places_graph = create_graph_fromfile(file, places_graph, colors_place, colorsheme)
+    
     places_graph.render(("output/"+outputfile), view=True) 
     os.remove("output/"+outputfile)
 
